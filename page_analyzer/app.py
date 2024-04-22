7eb32857a50696372b51df10b4120f258dc6bfe6
LuybovB <grandprinceess7777@gmail.com>
2024-04-22 21:07:19 +0300

    1step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Retrieve the PORT environment variable or use 5000 as a default
+port = int(os.environ.get("PORT", 5000))
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    # Run the app on the specified port
+    app.run(host="0.0.0.0", port=port)
